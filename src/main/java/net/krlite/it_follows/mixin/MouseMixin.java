8ef818c3ee03ba17271a641d56b622192a7ccbe8
KrLite <68179735+KrLite@users.noreply.github.com>
2024-01-01 21:46:04 +0800

    mouse moved.

--- src/main/java/net/krlite/it_follows/mixin/MouseMixin.java
+++ src/main/java/net/krlite/it_follows/mixin/MouseMixin.java
@@ -3,7 +3,9 @@ package net.krlite.it_follows.mixin;
 import net.krlite.it_follows.ItFollows;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
+import net.minecraft.client.util.InputUtil;
 import org.joml.Vector2i;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,14 +14,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Mouse.class)
 public abstract class MouseMixin {
-    @Shadow protected abstract void onCursorPos(long window, double x, double y);
+    @Shadow private double x;
+
+    @Shadow private double y;
+
+    @Shadow @Final private MinecraftClient client;
 
     @Inject(method = "onResolutionChanged", at = @At("RETURN"))
     private void onResolutionChanged(CallbackInfo ci) {
         Vector2i pos = ItFollows.buttonPos();
         if (pos != null) {
-            onCursorPos(MinecraftClient.getInstance().getWindow().getHandle(), pos.x(), pos.y());
-            System.out.println(pos.x() + ", " + pos.y());
+            x = pos.x();
+            y = pos.y();
+            InputUtil.setCursorParameters(client.getWindow().getHandle(), 212993, x, y);
         }
     }
 }
