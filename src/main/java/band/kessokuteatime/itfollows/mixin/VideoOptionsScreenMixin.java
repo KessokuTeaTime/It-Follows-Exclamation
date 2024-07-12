ed8fbdfab6567865792865d739c62414729738be
KrLite <68179735+KrLite@users.noreply.github.com>
2024-07-12 13:18:24 +0800

    for 1.20.5

--- src/main/java/band/kessokuteatime/itfollows/mixin/VideoOptionsScreenMixin.java
+++ src/main/java/band/kessokuteatime/itfollows/mixin/VideoOptionsScreenMixin.java
@@ -9,21 +9,24 @@ import net.minecraft.client.gui.widget.OptionListWidget;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(VideoOptionsScreen.class)
 public abstract class VideoOptionsScreenMixin extends GameOptionsScreen {
+    @Shadow private OptionListWidget list;
+
     public VideoOptionsScreenMixin(Screen parent, GameOptions gameOptions, Text title) {
         super(parent, gameOptions, title);
     }
 
-    @Inject(method = "addOptions", at = @At("RETURN"))
+    @Inject(method = "init", at = @At("RETURN"))
     private void init(CallbackInfo ci) {
-        assert body != null;
+        assert list != null;
 
-        var widget = (CyclingButtonWidget<?>) body.children().get(6).children().getFirst();
+        var widget = (CyclingButtonWidget<?>) list.children().get(6).children().getFirst();
         ItFollows.foundWidget(widget);
         ItFollows.fetchXFromWidget(widget);
     }
